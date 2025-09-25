766c5d79a8415fdd63bdecae92ba1085d4020488
Jaqueline Silva Santos <jaqueline.3001ss@gmail.com>
2025-09-25 18:25:23 -0300

    Add files via upload

---
+++ Aula.2-4.1.py
@@ -0,0 +1,9 @@
+# Usando os Operadores Básicos
+
+a = int(input('Digite um número inteiro: '))
+
+b = int(input('Digite outro número inteiro: '))
+
+soma = a + b
+
+print(f'A soma dos números {a}+{b} = {soma}')
